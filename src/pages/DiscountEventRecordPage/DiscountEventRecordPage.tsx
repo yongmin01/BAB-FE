5b73edaee03356c141c6c367855ba7bd6ed3c9cf
Jayjunyoung <no2jfamily@tukorea.ac.kr>
2024-08-10 20:47:27 +0900

    design: 진행했던 할인행사 정보 없을 시 페이지 퍼블리싱

--- src/pages/DiscountEventRecordPage/DiscountEventRecordPage.tsx
+++ src/pages/DiscountEventRecordPage/DiscountEventRecordPage.tsx
@@ -15,6 +15,7 @@ import { useNavigate } from 'react-router-dom'
 import storeInfoStore from '@stores/storeInfoStore'
 import discountEventStore from '@stores/discountEventStore'
 import { fetchDiscountEvents } from '@apis/Discount/fetchDiscountEvents'
+import EmptyState from '@components/EmptyState'
 
 export default function DiscountEventRecordPage() {
   const navigate = useNavigate()
@@ -23,13 +24,15 @@ export default function DiscountEventRecordPage() {
   const discountEvents = discountEventStore.getState().discountEvents
 
   useEffect(() => {
-    if (storeInfos.length > 0 && storeInfos[0].id) {
+    if (storeInfos.length && storeInfos[0].id) {
       fetchDiscountEvents(storeInfos[0].id).then(() => {
         console.log(
           'Fetched Discount Events:',
           discountEventStore.getState().discountEvents,
         )
       })
+    } else {
+      return
     }
   }, [storeInfos])
 
@@ -38,26 +41,29 @@ export default function DiscountEventRecordPage() {
   } //여기 API 코드는 할인정보 삭제 브렌치 파서 진행하겠습니다(본격적인 할인 로직 다룰때)
   //현재는 클라이언트 단에서(스토어) 삭제되는거까지만 해놨습니다.
   return (
-    <PageContainer>
+    <PageContainer events={discountEvents.length > 0 ? 'true' : 'false'}>
       <Header>
         <BackButton onClick={() => navigate('/manager')}>&lt;</BackButton>
         <Title>진행했던 할인행사 보기</Title>
       </Header>
-      <EventList>
-        {discountEvents.map((event) => (
-          <EventItem key={event.discountId}>
-            <EventTitle>{event.storeName}</EventTitle>{' '}
-            {/* storeName을 렌더링 */}
-            <EventDescription>{event.discountTitle}</EventDescription>
-            <EventPeriod>
-              {event.startDate} ~ {event.endDate}
-            </EventPeriod>
-            <DeleteButton onClick={() => handleDeleteClick(event.discountId)}>
-              삭제
-            </DeleteButton>
-          </EventItem>
-        ))}
-      </EventList>
+      {discountEvents.length > 0 ? (
+        <EventList>
+          {discountEvents.map((event) => (
+            <EventItem key={event.discountId}>
+              <EventTitle>{event.storeName}</EventTitle>
+              <EventDescription>{event.discountTitle}</EventDescription>
+              <EventPeriod>
+                {event.startDate} ~ {event.endDate}
+              </EventPeriod>
+              <DeleteButton onClick={() => handleDeleteClick(event.discountId)}>
+                삭제
+              </DeleteButton>
+            </EventItem>
+          ))}
+        </EventList>
+      ) : (
+        <EmptyState />
+      )}
     </PageContainer>
   )
 }
