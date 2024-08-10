62e6af9393f1efdb819b1bdec92c3d751e86e2c5
Jayjunyoung <no2jfamily@tukorea.ac.kr>
2024-08-10 10:14:41 +0900

    remove: 필요없는 주석 제거 및 파일 제거

--- src/apis/Store/deleteStore.ts
+++ src/apis/Store/deleteStore.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL
-//기본 엔드포인트는 추후 env에다가 설정해두는게 좋을 거 같네요
+
 export const deleteStore = async (storeId: number) => {
   try {
     const response = await axios.delete(`${API_BASE_URL}/v1/stores/${storeId}`)
