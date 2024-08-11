604d71f2d5a44ed4f51127daa35095d3ee51ecc7
solmee <kii5541@naver.com>
2024-08-12 03:34:41 +0900

    feat: useDebounce훅 구현 및 eslint에서 any error를 warn으로 변경

---
+++ src/hooks/useDebounce.ts
@@ -0,0 +1,15 @@
+export default function useDebounce<T extends (...args: any[]) => void>(
+  func: T,
+  delay: number,
+): (...args: Parameters<T>) => void {
+  let timeoutId: ReturnType<typeof setTimeout> | undefined
+
+  return function (...args: Parameters<T>) {
+    if (timeoutId) {
+      clearTimeout(timeoutId)
+    }
+    timeoutId = setTimeout(() => {
+      func(...args)
+    }, delay)
+  }
+}
