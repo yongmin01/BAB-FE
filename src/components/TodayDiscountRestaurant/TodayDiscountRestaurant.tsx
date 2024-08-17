f9b11eead91bd206550f94475cee1740d91f4ce2
yongmin01 <heart77123@naver.com>
2024-08-18 02:37:09 +0900

    feat: 오늘의 할인 식당 페이지 api 연결 및 라우팅 설정

--- src/components/TodayDiscountRestaurant/TodayDiscountRestaurant.tsx
+++ src/components/TodayDiscountRestaurant/TodayDiscountRestaurant.tsx
@@ -11,79 +11,58 @@ import {
   DiscountedPrice,
   Price,
 } from './TodayDiscountRestaurant.style'
+import { useEffect } from 'react'
+import { getTodayDiscountRestaurants } from '@apis/getTodayDiscountRestaurants'
+import { useTodayDiscountStore } from '@stores/todayDiscountRestaurantsInfoStore'
+import { useNavigate } from 'react-router-dom'
 
 export default function TodayDiscountRestaurant() {
-  const dummy = [
-    {
-      id: 1,
-      discountType: 'SKT 할인',
-      restaurantName: '스타벅스',
-      menu: [
-        {
-          id: 1,
-          dishName: '바닐라 빈 푸딩 블렌디드 위드 에스프레소',
-          discountedPrice: 8300,
-          price: 9000,
-        },
-        {
-          id: 2,
-          dishName: '퍼플 드링크 위드 망고 용과 소다',
-          discountedPrice: 7300,
-          price: 8000,
-        },
-      ],
-    },
+  const navigator = useNavigate()
+  const discountRestaurantList = useTodayDiscountStore(
+    (state) => state.discounts,
+  )
+  useEffect(() => {
+    const request = async () => {
+      try {
+        const res = await getTodayDiscountRestaurants()
+        if (res) {
+          console.log('Data loaded successfully:', res)
+        }
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    request()
+  }, [])
 
-    {
-      id: 3,
-      discountType: '가게 특별 할인',
-      restaurantName: '샐러디',
-      menu: [
-        {
-          id: 1,
-          dishName: '콥 샐러드',
-          discountedPrice: 7200,
-          price: 8900,
-        },
-        {
-          id: 2,
-          dishName: '탄단지 샐러드',
-          discountedPrice: 7200,
-          price: 8900,
-        },
-      ],
-    },
-  ]
   return (
     <RestaurantList>
-      {dummy.map((restaurant) => (
-        <Restaurant key={restaurant.id}>
-          <DiscountType>{restaurant.discountType}</DiscountType>
-          <RestaurantName>{restaurant.restaurantName}</RestaurantName>
-          <GoToRestaurantBtn>
-            바로 가기
-            <span>&gt;</span>
-          </GoToRestaurantBtn>
-          <MenuList>
-            <Menu>
-              <Dish>{restaurant.menu[0].dishName}</Dish>
-              <Dash />
-              <DiscountedPrice>
-                {restaurant.menu[0].discountedPrice}
-              </DiscountedPrice>
-              <Price>{restaurant.menu[0].price}</Price>
-            </Menu>
-            <Menu>
-              <Dish>{restaurant.menu[1].dishName}</Dish>
-              <Dash />
-              <DiscountedPrice>
-                {restaurant.menu[1].discountedPrice}
-              </DiscountedPrice>
-              <Price>{restaurant.menu[1].price}</Price>
-            </Menu>
-          </MenuList>
-        </Restaurant>
-      ))}
+      {discountRestaurantList.length > 0 ? (
+        discountRestaurantList.map((restaurant) => (
+          <Restaurant key={restaurant.discountId}>
+            <DiscountType>{restaurant.discountTitle}</DiscountType>
+            <RestaurantName>{restaurant.storeName}</RestaurantName>
+            <GoToRestaurantBtn
+              onClick={() => navigator(`/shopdetail/${restaurant.storeId}`)}
+            >
+              바로 가기
+              <span>&gt;</span>
+            </GoToRestaurantBtn>
+            <MenuList>
+              {restaurant.getOnSaleStoreMenuDataDtoList.map((menu) => (
+                <Menu>
+                  <Dish>{menu.menuName}</Dish>
+                  <Dash />
+                  <DiscountedPrice>{menu.discountPrice}</DiscountedPrice>
+                  <Price>{menu.price}</Price>
+                </Menu>
+              ))}
+            </MenuList>
+          </Restaurant>
+        ))
+      ) : (
+        <div>없음</div>
+      )}
     </RestaurantList>
   )
 }
