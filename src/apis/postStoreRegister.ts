cb55bea67840180976cfcb53f1858a9818afab56
solmee <87332713+ssolfa@users.noreply.github.com>
2024-08-18 02:57:08 +0900

    (#32)feat/store register api (#40)
    * feat: 가게 등록 api 연결
    
    * feat: useDebounce훅 구현 및 eslint에서 any error를 warn으로 변경
    
    * feat: 대학 정보 가져오기 api 연결 및 대학 검색 컴포넌트 구현
    
    * feat: 가게 등록 첫 페이지에 대학 검색 컴포넌트로 교체
    
    * chore: 버튼 스타일 수정
    
    * feat: 가게 등록 페이지에 정보 등록 api 연결 및 검색 후 스크롤 위치 이동 구현
    
    * fix: useDebounce훅 수정
    
    * fix: 가게 등록 페이지에 수정한 useDebounce 훅 적용
    
    * feat: store에 가게 정보 저장 구현

---
+++ src/apis/postStoreRegister.ts
@@ -0,0 +1,58 @@
+import axios, { AxiosResponse } from 'axios'
+
+const API = import.meta.env.VITE_API_BASE_URL
+
+interface StoreData {
+  name: string
+  longitude: number
+  latitude: number
+  address: string
+  streetAddress: string
+  storeLink: string
+  registration: string
+  university: string
+}
+
+interface ApiResponse {
+  isSuccess: boolean
+  code: string
+  message: string
+  result: {
+    id: number
+    name: string
+    bannerImageUrls: string[]
+  }
+}
+
+export const postStoreRegister = async (
+  StoreData: StoreData,
+  token: string,
+  bannerFiles?: File[],
+) => {
+  try {
+    const formData = new FormData()
+    formData.append('requestDto', JSON.stringify(StoreData))
+
+    if (bannerFiles) {
+      bannerFiles.forEach((file) => {
+        formData.append('bannerFiles', file)
+      })
+    }
+
+    const response: AxiosResponse<ApiResponse> = await axios.post(
+      `${API}/v1/stores`,
+      formData,
+      {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+          Authorization: `Bearer ${token}`,
+        },
+      },
+    )
+
+    return response.data
+  } catch (error) {
+    console.error(error)
+    throw error
+  }
+}
