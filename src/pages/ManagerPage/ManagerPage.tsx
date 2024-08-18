ee39f0e4f9d6743a4483bedc093f4a14787ada0f
solmee <kii5541@naver.com>
2024-08-19 00:14:06 +0900

    feat: managerpage에 api 연결 및 구조 변경

--- src/pages/ManagerPage/ManagerPage.tsx
+++ src/pages/ManagerPage/ManagerPage.tsx
@@ -1,106 +1,122 @@
+import { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
 import RegistrationPrompt from '@components/RegistrationPrompt'
-import { ManagerRegisterState } from '@stores/managerRegisterInfoStore'
-import managerRegisterInfoStore from '@stores/managerRegisterInfoStore'
-import { ManagerPageContainer } from './ManagerPage.style'
+import ManagerCompletedCard from '@components/ManagerCompletedCard/ManagerCompletedCard'
+import MyPageCardAccount from '@components/MyPageCard/Account/MyPageCardAccount'
+import DiscountModal from '@components/Modal/DiscountModal'
+import HeaderTitle from '@components/HeaderTitle/HeaderTitle'
 import icon from '@assets/managerMypage/등록증 아이콘.svg'
 import menuIcon from '@assets/managerMypage/메뉴아이콘.svg'
-import useModalStore from '@stores/modalStore'
-import { useEffect, useState } from 'react'
-import DiscountModal from '@components/Modal/DiscountModal'
-import ManagerCompletedCard from '@components/ManagerCompletedCard/ManagerCompletedCard'
-import storeInfoStore, { StoreInfo } from '@stores/storeInfoStore'
-import discountEventStore from '@stores/discountEventStore'
-import { generateUniqueId } from '@utils/generateUniqueId'
-import { useNavigate } from 'react-router-dom'
-import HeaderTitle from '@components/HeaderTitle/HeaderTitle'
-import MyPageCardAccount from '@components/MyPageCard/Account/MyPageCardAccount'
-import { StyledCard } from '@components/MyPageCard/MyPageCard.style'
+import ArrowImg from '@assets/StudentPage/arrow.svg'
 import {
   ImgBtnContainer,
   Btn,
   StyledArrow,
 } from '@components/MyPageCard/MyPageCardTop/MyPageCardTop.style'
-import ArrowImg from '@assets/StudentPage/arrow.svg'
+import { StyledCard } from '@components/MyPageCard/MyPageCard.style'
+import { ManagerPageContainer } from './ManagerPage.style'
+import axios from 'axios'
+import managerRegisterInfoStore from '@stores/managerRegisterInfoStore'
 
 export default function ManagerPage() {
   const navigate = useNavigate()
-  const { isRegistered, setIsRegistered, setManagerRegistrationInfo } =
+  const { isRegistered, isStoreRegistered, ownerNickname, updateFromApi } =
     managerRegisterInfoStore()
-  const { isStoreRegistered, setStoreRegistered } = storeInfoStore()
-  const storeInfos = storeInfoStore((state) => state.storeInfos)
-  const discountEvents = discountEventStore.getState().discountEvents
-  const { openModal } = useModalStore()
-  const [businessData, setBusinessData] = useState<ManagerRegisterState | null>(
-    null,
-  )
 
-  useEffect(() => {
-    if (isRegistered) {
-      setBusinessData(managerRegisterInfoStore.getState())
-    } else {
-      setBusinessData(null)
+  const fetchOwnerData = async () => {
+    try {
+      const response = await axios.get(
+        `${import.meta.env.VITE_API_BASE_URL}/v1/users/owner/mypage`,
+        {
+          headers: {
+            Authorization: `Bearer ${import.meta.env.VITE_APP_API_TOKEN}`,
+          },
+        },
+      )
+
+      if (response.data.isSuccess) {
+        const { ownerId, ownerNickname, storeId, storeName } =
+          response.data.result
+
+        updateFromApi({
+          ownerId,
+          ownerNickname,
+          storeId,
+          storeName,
+        })
+      } else {
+        console.error(
+          'Failed to fetch owner mypage data:',
+          response.data.message,
+        )
+      }
+    } catch (error) {
+      console.error('사장님 마이페이지 오류', error)
     }
-  }, [isRegistered])
+  }
 
-  useEffect(() => {
-    console.log('Updated Store Infos:', storeInfos)
-  }, [storeInfos])
-
-  const handleManagerRegisterClick = (): void => {
-    setIsRegistered(true)
-    setManagerRegistrationInfo({
-      managerName: '고서현',
-      isRegistered: true,
-      registrationNumber: 12345678,
-      businessName: '예제 사업자명',
-      businessAddress: '예제 주소',
-      industry: '업태',
-      item: '종목',
-    })
+  const handleManagerRegisterClick = async () => {
+    await fetchOwnerData()
     navigate('/businessdocupload')
   }
 
-  const handleStoreRegisterClick = (): void => {
-    setStoreRegistered(true)
-    openModal()
+  const handleStoreRegisterClick = async () => {
+    await fetchOwnerData()
+    navigate('/firstregisterstoreinfo')
   }
+
+  useEffect(() => {
+    fetchOwnerData()
+  }, [])
+
+  const renderRegisteredContent = () => (
+    <>
+      <RegistrationPrompt
+        isRegistered={true}
+        businessData={[managerRegisterInfoStore.getState()]}
+        ownerNickname={ownerNickname}
+      />
+      <ImgBtnContainer $gap="12px">
+        <img src={menuIcon} alt="메뉴 아이콘" />
+        <Btn onClick={handleStoreRegisterClick} $padding="18px">
+          가게 등록하러 가기
+          <StyledArrow src={ArrowImg} />
+        </Btn>
+      </ImgBtnContainer>
+    </>
+  )
+
+  const renderUnregisteredContent = () => (
+    <>
+      <RegistrationPrompt
+        ownerNickname={ownerNickname}
+        isRegistered={false}
+        businessData={null}
+      />
+      <ImgBtnContainer $gap="18px">
+        <img src={icon} alt="등록증 아이콘" />
+        <Btn onClick={handleManagerRegisterClick} $padding="18px">
+          사업자 정보 등록하기
+          <StyledArrow src={ArrowImg} />
+        </Btn>
+      </ImgBtnContainer>
+    </>
+  )
+
   return (
     <ManagerPageContainer>
       <HeaderTitle title="마이페이지" $icon="notification" />
-
       {isStoreRegistered ? (
         <ManagerCompletedCard />
       ) : (
         <StyledCard $paddingtop="35px" $paddingbottom="26px">
-          <RegistrationPrompt
-            isRegistered={isRegistered}
-            businessData={businessData ? [businessData] : null}
-          />
-          {isRegistered && businessData ? (
-            <>
-              <ImgBtnContainer $gap="12px">
-                <img src={menuIcon} alt="메뉴 아이콘" />
-                <Btn onClick={handleStoreRegisterClick} $padding="18px">
-                  가게 등록하러 가기
-                  <StyledArrow src={ArrowImg} />
-                </Btn>
-              </ImgBtnContainer>
-            </>
-          ) : (
-            <>
-              <ImgBtnContainer $gap="18px">
-                <img src={icon} alt="등록증 아이콘" />
-                <Btn onClick={handleManagerRegisterClick} $padding="18px">
-                  사업자 정보 등록하기
-                  <StyledArrow src={ArrowImg} />
-                </Btn>
-              </ImgBtnContainer>
-            </>
-          )}
+          {isRegistered
+            ? renderRegisteredContent()
+            : renderUnregisteredContent()}
         </StyledCard>
       )}
       <MyPageCardAccount />
-      {isStoreRegistered ? <DiscountModal /> : null}
+      {isStoreRegistered && <DiscountModal />}
     </ManagerPageContainer>
   )
 }
