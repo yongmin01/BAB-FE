3e5361711290038dc36b8f2d5b9c7f8a30aec58b
yongmin01 <heart77123@naver.com>
2024-08-18 16:16:39 +0900

    fix: store 네이밍 수정 및 persist 옵션 제거

--- src/stores/studentInfoStore.ts
+++ src/stores/studentInfoStore.ts
@@ -1,23 +1,19 @@
 import { create } from 'zustand'
-import { persist } from 'zustand/middleware'
 
 interface StudentInfo {
   studentName: string
-  id: string
+  accountId: string
   isSchoolSet: boolean
+  setStudentName: (studentName: string) => void
+  setAccountId: (accountId: string) => void
   setIsSchoolSet: (school: boolean) => void
 }
 
-export const studentInfoStore = create(
-  persist<StudentInfo>(
-    (set) => ({
-      studentName: '조용민',
-      id: 'yongii',
-      isSchoolSet: false,
-      setIsSchoolSet: (school) => set({ isSchoolSet: school }),
-    }),
-    {
-      name: 'isSchoolSet',
-    },
-  ),
-)
+export const useStudentInfoStore = create<StudentInfo>((set) => ({
+  studentName: '',
+  accountId: '',
+  isSchoolSet: false,
+  setStudentName: (studentName) => set({ studentName: studentName }),
+  setAccountId: (accountId) => set({ accountId: accountId }),
+  setIsSchoolSet: (school) => set({ isSchoolSet: school }),
+}))
