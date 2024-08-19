7ff7fd9d98f4df7bb342f351847e09a0ba3cd375
solmee <kii5541@naver.com>
2024-08-20 03:47:59 +0900

    feat: svg 추가 및 페이지에 따라 하단 네비게이션 바 변경 구현

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,23 +2,49 @@ import { FooterContainer } from './Footer.style'
 import { FooterIcon } from './Footer.style'
 import { FooterLink } from './Footer.style'
 import { FooterText } from './Footer.style'
-import homeIcon from '@icons/아이콘_홈.svg'
-import listIcon from '@icons/아이콘_할인가게.svg'
-import myPageIcon from '@icons/아이콘_마이페이지.svg'
+import homeIcon from '@icons/아이콘_홈_활성화.svg'
+import listIcon from '@icons/아이콘_할인가게_활성화.svg'
+import mypageIcon from '@icons/아이콘_마이페이지_활성화.svg'
+import noneHomeIcon from '@icons/아이콘_홈.svg'
+import noneListIcon from '@icons/아이콘_할인가게.svg'
+import noneMypageIcon from '@icons/아이콘_마이페이지.svg'
+import { useLocation } from 'react-router-dom'
 
 export default function Footer() {
+  const location = useLocation()
+
+  const getIcon = (
+    path: string,
+    activeIcon: string,
+    inactiveIcon: string,
+  ): string => {
+    return location.pathname === path ? activeIcon : inactiveIcon
+  }
+
   return (
     <FooterContainer>
-      <FooterLink to="/" className="active">
-        <FooterIcon src={homeIcon} alt="홈화면" />
+      <FooterLink to="/" className={location.pathname === '/' ? 'active' : ''}>
+        <FooterIcon src={getIcon('/', homeIcon, noneHomeIcon)} alt="홈화면" />
         <FooterText>홈화면</FooterText>
       </FooterLink>
-      <FooterLink to="/list">
-        <FooterIcon src={listIcon} alt="할인 가게 목록" />
+      <FooterLink
+        to="/list"
+        className={location.pathname === '/list' ? 'active' : ''}
+      >
+        <FooterIcon
+          src={getIcon('/list', listIcon, noneListIcon)}
+          alt="할인 가게 목록"
+        />
         <FooterText>할인 가게 목록</FooterText>
       </FooterLink>
-      <FooterLink to="/manager">
-        <FooterIcon src={myPageIcon} alt="마이페이지" />
+      <FooterLink
+        to="/manager"
+        className={location.pathname === '/manager' ? 'active' : ''}
+      >
+        <FooterIcon
+          src={getIcon('/manager', mypageIcon, noneMypageIcon)}
+          alt="마이페이지"
+        />
         <FooterText>마이페이지</FooterText>
       </FooterLink>
     </FooterContainer>
