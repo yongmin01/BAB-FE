12187bd89340f2593cccadd9e13c966decc482c2
solmee <kii5541@naver.com>
2024-08-20 04:09:37 +0900

    feat: 라우팅 경로 map과 shopdetail 추가

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -13,38 +13,43 @@ import { useLocation } from 'react-router-dom'
 export default function Footer() {
   const location = useLocation()
 
-  const getIcon = (
-    path: string,
-    activeIcon: string,
-    inactiveIcon: string,
-  ): string => {
-    return location.pathname === path ? activeIcon : inactiveIcon
+  const icons = {
+    home: location.pathname === '/map' ? homeIcon : noneHomeIcon,
+    list: ['/list', '/shopdetail'].includes(location.pathname)
+      ? listIcon
+      : noneListIcon,
+    mypage: ['/manager', '/studentPage'].includes(location.pathname)
+      ? mypageIcon
+      : noneMypageIcon,
   }
 
   return (
     <FooterContainer>
-      <FooterLink to="/" className={location.pathname === '/' ? 'active' : ''}>
-        <FooterIcon src={getIcon('/', homeIcon, noneHomeIcon)} alt="홈화면" />
+      <FooterLink
+        to="/map"
+        className={location.pathname === '/map' ? 'active' : ''}
+      >
+        <FooterIcon src={icons.home} alt="홈화면" />
         <FooterText>홈화면</FooterText>
       </FooterLink>
       <FooterLink
         to="/list"
-        className={location.pathname === '/list' ? 'active' : ''}
+        className={
+          ['/list', '/shopdetail'].includes(location.pathname) ? 'active' : ''
+        }
       >
-        <FooterIcon
-          src={getIcon('/list', listIcon, noneListIcon)}
-          alt="할인 가게 목록"
-        />
+        <FooterIcon src={icons.list} alt="할인 가게 목록" />
         <FooterText>할인 가게 목록</FooterText>
       </FooterLink>
       <FooterLink
         to="/manager"
-        className={location.pathname === '/manager' ? 'active' : ''}
+        className={
+          ['/manager', '/studentPage'].includes(location.pathname)
+            ? 'active'
+            : ''
+        }
       >
-        <FooterIcon
-          src={getIcon('/manager', mypageIcon, noneMypageIcon)}
-          alt="마이페이지"
-        />
+        <FooterIcon src={icons.mypage} alt="마이페이지" />
         <FooterText>마이페이지</FooterText>
       </FooterLink>
     </FooterContainer>
